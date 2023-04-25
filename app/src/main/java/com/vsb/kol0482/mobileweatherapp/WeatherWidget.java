bfe6501f43a3df8ca93586d21ea2467a3016c2e7
Just-aName <64785898+Just-aName@users.noreply.github.com>
2023-04-25 20:12:18 +0200

    widget settings

--- app/src/main/java/com/vsb/kol0482/mobileweatherapp/WeatherWidget.java
+++ app/src/main/java/com/vsb/kol0482/mobileweatherapp/WeatherWidget.java
@@ -48,13 +48,14 @@ public class WeatherWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
 
+            Intent settingIntent5 = new Intent(context, Settings.class);
+            PendingIntent pendingIntent5 = PendingIntent.getActivity(context, 5, settingIntent5, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setOnClickPendingIntent(R.id.header_button_2, pendingIntent5);
+
+
             getWeatherInfo(context, new WeatherInfoCallback() {
                 @Override
                 public void onSuccess(JSONObject data) {
-                    Intent settingIntent = new Intent(context, Settings.class);
-                    PendingIntent pendingIntent5 = PendingIntent.getActivity(context, 5, settingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-                    views.setOnClickPendingIntent(R.id.header_layout, pendingIntent5);
-
                     // Set up the first tile
                     Intent intent1 = new Intent(context, GraphActivity.class);
                     intent1.putExtra("SelectedUnit", WidgetSettings.GetLeftUp());
@@ -162,74 +163,7 @@ public class WeatherWidget extends AppWidgetProvider {
                     // Handle the error
                     // ...
                     Log.d("WIDGET", "Widget data load ERROR!");
-                    Intent settingIntent = new Intent(context, Settings.class);
-                    PendingIntent pendingIntent5 = PendingIntent.getActivity(context, 5, settingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-                    views.setOnClickPendingIntent(R.id.header_layout, pendingIntent5);
-
-                    // Set up the first tile
-                    Intent intent1 = new Intent(context, GraphActivity.class);
-                    intent1.putExtra("SelectedUnit", WidgetSettings.GetLeftUp());
-                    PendingIntent pendingIntent1 = PendingIntent.getActivity(context, 1, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
-                    views.setOnClickPendingIntent(R.id.tile1, pendingIntent1);
-
-                    // Set up the second tile
-                    Intent intent2 = new Intent(context, GraphActivity.class);
-                    intent2.putExtra("SelectedUnit", WidgetSettings.GetRightUp());
-                    PendingIntent pendingIntent2 = PendingIntent.getActivity(context, 2, intent2, PendingIntent.FLAG_UPDATE_CURRENT);
-                    views.setOnClickPendingIntent(R.id.tile2, pendingIntent2);
-
-                    // Set up the third tile
-                    Intent intent3 = new Intent(context, GraphActivity.class);
-                    intent3.putExtra("SelectedUnit", WidgetSettings.GetLeftDown());
-                    PendingIntent pendingIntent3 = PendingIntent.getActivity(context, 3, intent3, PendingIntent.FLAG_UPDATE_CURRENT);
-                    views.setOnClickPendingIntent(R.id.tile3, pendingIntent3);
-
-                    // Set up the fourth tile
-                    Intent intent4 = new Intent(context, GraphActivity.class);
-                    intent4.putExtra("SelectedUnit", WidgetSettings.GetRightDown());
-                    PendingIntent pendingIntent4 = PendingIntent.getActivity(context, 4, intent4, PendingIntent.FLAG_UPDATE_CURRENT);
-                    views.setOnClickPendingIntent(R.id.tile4, pendingIntent4);
-
-                    // Update the text for each tile
-                    if(WidgetSettings.GetLeftUp().equals(WidgetOptions.NONE.getValue()))
-                    {
-                        views.setViewVisibility(R.id.tile1, View.GONE);
-                    }
-                    else {
-                        views.setViewVisibility(R.id.tile1, View.VISIBLE);
-                        views.setTextViewText(R.id.tile1_text, "DATA ERROR");
-                        views.setTextViewText(R.id.tile1_title, WidgetSettings.GetLeftUp());
-                    }
-
-                    if(WidgetSettings.GetRightUp().equals(WidgetOptions.NONE.getValue()))
-                    {
-                        views.setViewVisibility(R.id.tile2, View.GONE);
-                    }
-                    else {
-                        views.setViewVisibility(R.id.tile2, View.VISIBLE);
-                        views.setTextViewText(R.id.tile2_text, "DATA ERROR");
-                        views.setTextViewText(R.id.tile2_title, WidgetSettings.GetRightUp());
-                    }
-
-                    if(WidgetSettings.GetLeftDown().equals(WidgetOptions.NONE.getValue()))
-                    {
-                        views.setViewVisibility(R.id.tile3, View.GONE);
-                    }
-                    else {
-                        views.setViewVisibility(R.id.tile3, View.VISIBLE);
-                        views.setTextViewText(R.id.tile3_text, "DATA ERROR");
-                        views.setTextViewText(R.id.tile3_title, WidgetSettings.GetLeftDown());
-                    }
-
-                    if(WidgetSettings.GetRightDown().equals(WidgetOptions.NONE.getValue()))
-                    {
-                        views.setViewVisibility(R.id.tile4, View.GONE);
-                    }
-                    else {
-                        views.setViewVisibility(R.id.tile4, View.VISIBLE);
-                        views.setTextViewText(R.id.tile4_text, "DATA ERROR");
-                        views.setTextViewText(R.id.tile4_title, WidgetSettings.GetRightDown());
-                    }
+                    error.printStackTrace();
                 }
             });
 
@@ -245,6 +179,7 @@ public class WeatherWidget extends AppWidgetProvider {
     public void onEnabled(Context context) {
         super.onEnabled(context);
         WidgetSettings.initWidgetSettings(context);
+
     }
 
     @Override
